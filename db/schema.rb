b17acdfc44761421029e3ac07be15dbd5a007cc9
Isaac Andriamiharisoa <nyainaisaac@gmail.com>
2023-11-01 11:32:05 +0400

    image url added to planet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_01_072150) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_01_072822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_01_072150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "temperature"
+    t.string "image_url", default: ""
   end
 
   create_table "users", force: :cascade do |t|
