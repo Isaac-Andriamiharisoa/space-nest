b2b466d6a935025f7eb6d413e284bb28347cd3e1
Isaac Andriamiharisoa <nyainaisaac@gmail.com>
2023-11-01 11:45:14 +0400

    added temp to index planet

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,25 @@
 +-class BookingsController < ApplicationController
-  def new
-    @booking = Booking.new
-    # @user = current_user
-    @planet = Planet.find(params[:planet_id])
-  end
+    def new
+      @booking = Booking.new
+      # @user = current_user
+      @planet = Planet.find(params[:planet_id])
+    end
 
-  def create
-    @booking = Booking.create(booking_params)
-    @booking.user = current_user
-    @planet = Planet.find(params[:planet_id])
-    @booking.planet = @planet
-    if @booking.save
-      redirect_to planet_path(@planet)
-    else
-      render :new, status: :unprocessable_entity
+    def create
+      @booking = Booking.create(booking_params)
+      @booking.user = current_user
+      @planet = Planet.find(params[:planet_id])
+      @booking.planet = @planet
+      if @booking.save
+        redirect_to planet_path(@planet)
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def booking_params
+      params.require(:booking).permit(:planet_id, :user_id, :start_date, :end_date)
     end
   end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:planet_id, :user_id, :start_date, :end_date)
-  end
-end
