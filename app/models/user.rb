e9cca89740661642a38ff7c524e7046f8cdb85ee
Isaac Andriamiharisoa <nyainaisaac@gmail.com>
2023-11-03 10:36:14 +0400

    user photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  def upload_image_to_cloudinary
+    Cloudinary::Uploader.upload(photo.path, public_id: "user#{current_user.id}")
+  end
 end
