afc9390306deae3eaa837f6cfd4db3492ddb957f
Dylan Therese <dylantherese@outlook.com>
2024-11-24 18:36:52 +0400

    minor fixes

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,9 +1,9 @@
 class Planet < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_one_attached :photo
+  # has_one_attached :photo
 
-  def upload_image_to_cloudinary
-    Cloudinary::Uploader.upload(photo.path, public_id: name)
-  end
+  # def upload_image_to_cloudinary
+  #   Cloudinary::Uploader.upload(photo.path, public_id: name)
+  # end
 end
